4fad4723297b9e212982ac400436b6817237ab6f
luisgranados07 <102258820+luisgranados07@users.noreply.github.com>
2022-03-23 17:55:37 -0600

    Add files via upload

---
+++ Clase_VIII/Clase_VIII_BLL/Generico/cls_ClaseVIII_BLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_VIII_BLL.Generico
+{
+    class cls_ClaseVIII_BLL
+    {
+    }
+}
